5d98a4baa22e72f0ee8a92184c51b62eddd1a8cf
Wayne Stewart <wstewart@bwm.com>
2017-05-25 21:20:37 -0400

    Still having issue trrying to get sumbit button to find the key code a letter. Tried eventlister on the submit button but did nor work out all that well. #8:15pm to 9:15pm

--- hangman/js/hangman.js
+++ hangman/js/hangman.js
@@ -2,33 +2,28 @@ var words = ['corvette','GTR','FocusRS', 'mazdaspeed,'];
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
 var guess = [];
 
+//Getting elements to be called later
+var submitLetter = document.getElementById("submit-button");
+var inputField = document.getElementById("input-letter");
+
+//Captureing Keypress in field
+document.getElementById("submit-button").addEventListener("click", function(){
+console.log("submit-button");
+});
 
 
 
-  var keypress = event.keycode;
-  document.getElementById("input-letter");
-
-
-
-console.log(keypress);
-
-//I guess create a function that will hold the code. Function hangman will be the name.
-function hangman(){
-  var findRandom = Math.floor(Math.random() * myword.length);
-  var pickWord =  myword[findRandom];
-  console.log(pickWord);
-  for (i = 0; i < pickWord.length; i++) {
-    if (pickWord[i] == guess) {
-      // console.log(pickWord);
-      // console.log(guess);
-      // console.log("Great job! Please select again");
-    }
-    else {
-        // console.log("Please try your selection again");
-    }
-  }
-}
-
-// console.log(hangman());
-//runs the function on the page
-hangman();
+// function hangman(){
+//   var findRandom = Math.floor(Math.random() * myword.length);
+//   var pickWord =  myword[findRandom];
+//   console.log(pickWord);
+//   for (i = 0; i < pickWord.length; i++) {
+//     if (pickWord[i] == guess) {
+//
+//     }
+//     else {
+//
+//     }
+//   }
+// }
+// hangman();
