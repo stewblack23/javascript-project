2fd43b6158e71a90b33abd156f45d78691bf89a4
Wayne Stewart <wsimon23@gmail.com>
2017-06-03 16:44:54 -0400

    Finally got Hangman game kinda working, Need to add amount of letters to guess.

--- hangman/js/hangman.js
+++ hangman/js/hangman.js
@@ -7,42 +7,24 @@ var guess = [];
 var guessLives = 6;
 
 //Used to display word on screen
-var testWord = "test";
 var displayWord = "";
 
 // function looks into words array and creates a random selection within the array
-function getWords() {
-    var findRandom = Math.floor(Math.random() * words.length);
-    var pickWord =  words[findRandom];
-}
+var findRandom = Math.floor(Math.random() * words.length);
+
+//randomizes words array and puts it into a function.
+var pickWord =  words[findRandom];
 
 //function calls getWords function and loops through guess array and compares it to words array
 function underlined() {
-  getWords()
-  for (i = 0; i < words.length; i++) {
+  for (i = 0; i < pickWord.length; i++) {
     //Getting the first charater in words array and comparing it to empty guess array
-    guess[i] = words.charAt(i);
-    console.log(guess);
+    guess[i] = pickWord.charAt(i);
+    console.log(pickWord);
     guess[i] = "_";
   }
   displayWord = guess.join("");
-  console.log(guess);
   document.getElementById("letters").innerHTML = displayWord;
 }
 
-underlined()
-
-// function hangman(){
-//   var findRandom = Math.floor(Math.random() * myword.length);
-//   var pickWord =  myword[findRandom];
-//   console.log(pickWord);
-//   for (i = 0; i < pickWord.length; i++) {
-//     if (pickWord[i] == guess) {
-//
-//     }
-//     else {
-//
-//     }
-//   }
-// }
-// hangman();
+underlined();
