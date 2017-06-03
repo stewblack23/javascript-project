ccfa2a410519fb8d6ef3b6aee0ea5c8cedfe8a55
Wayne Stewart <wsimon23@gmail.com>
2017-06-03 16:12:27 -0400

    Getting a little futher into writing the code. .charAt fucntion is not working correctly.

--- hangman/js/hangman.js
+++ hangman/js/hangman.js
@@ -6,24 +6,31 @@ var guess = [];
 //create variable for how many guesses the user receives
 var guessLives = 6;
 
-//.indexOf type will return the index
-
-//.split
+//Used to display word on screen
+var testWord = "test";
+var displayWord = "";
 
 // function looks into words array and creates a random selection within the array
 function getWords() {
     var findRandom = Math.floor(Math.random() * words.length);
     var pickWord =  words[findRandom];
-    console.log(pickWord);
 }
 
+//function calls getWords function and loops through guess array and compares it to words array
 function underlined() {
   getWords()
   for (i = 0; i < words.length; i++) {
-    
+    //Getting the first charater in words array and comparing it to empty guess array
+    guess[i] = words.charAt(i);
+    console.log(guess);
+    guess[i] = "_";
   }
+  displayWord = guess.join("");
+  console.log(guess);
+  document.getElementById("letters").innerHTML = displayWord;
 }
 
+underlined()
 
 // function hangman(){
 //   var findRandom = Math.floor(Math.random() * myword.length);
