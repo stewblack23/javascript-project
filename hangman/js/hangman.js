83e7b27ae136a0f712084f31a706caa90b50dad9
Wayne Stewart <wsimon23@gmail.com>
2017-06-05 22:20:46 -0400

    Trying to figure out the logic for the game. Plus new javascript project called image-change.

--- hangman/js/hangman.js
+++ hangman/js/hangman.js
@@ -1,21 +1,19 @@
-//create array called words
+//Array for words in game.
 var words = ['corvette','GTR','FocusRS', 'mazdaspeed', 'Accord'];
-//create array guess which is empty
+//Array will hold the amount of guess in the game.
 var guess = [];
 
 //create variable for how many guesses the user receives
-var guessLives = 6;
+var guessLives = 5;
 
 //Used to display word on screen
 var displayWord = "";
 
 // function looks into words array and creates a random selection within the array
 var findRandom = Math.floor(Math.random() * words.length);
-
-//randomizes words array and puts it into a function.
 var pickWord =  words[findRandom];
 
-//function calls getWords function and loops through guess array and compares it to words array
+//function loops through variable pickword and displays letters as underscore.
 function underlined() {
   for (i = 0; i < pickWord.length; i++) {
     //Getting the first charater in words array and comparing it to empty guess array
@@ -24,7 +22,27 @@ function underlined() {
     guess[i] = "_";
   }
   displayWord = guess.join("");
+  //Output function to the DOM
   document.getElementById("letters").innerHTML = displayWord;
+  document.getElementById("word-guess").innerHTML = pickWord.length;
+  console.log(pickWord.length);
 }
 
-underlined();
+//function that will allow us to
+function chooseLetter(letter) {
+  var changes = 0;
+  for (i = 0; i < pickWord.length; i++) {
+    guess[i] = pickWord.charAt(i);
+    if(pickWord.charAt(i) == letter) {
+      guess[i] == letter;
+      changes += 1;
+      console.log(changes);
+    }
+  }
+  if(changes < 1) {
+    guessLives -= 1;
+    document.getElementById("lives-amount").innerHTML = guessLives;
+  }
+  displayWord = pickWord.join("");
+  document.getElementById("letters").innerHTML = guessLives;
+}
